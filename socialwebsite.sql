94f193f4637d24282f272a7d5e8a2c831a955f38
vancuong2911 <100763155+vancuong2911@users.noreply.github.com>
2023-03-13 15:34:36 +0700

    Add files via upload

---
+++ socialwebsite.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 13, 2023 lúc 09:34 AM
+-- Phiên bản máy phục vụ: 10.4.27-MariaDB
+-- Phiên bản PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `socialwebsite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `posts`
+--
+-- Error reading structure for table socialwebsite.posts: #1932 - Table &#039;socialwebsite.posts&#039; doesn&#039;t exist in engine
+-- Error reading data for table socialwebsite.posts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `socialwebsite`.`posts`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+-- Error reading structure for table socialwebsite.users: #1932 - Table &#039;socialwebsite.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table socialwebsite.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `socialwebsite`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `verify`
+--
+-- Error reading structure for table socialwebsite.verify: #1932 - Table &#039;socialwebsite.verify&#039; doesn&#039;t exist in engine
+-- Error reading data for table socialwebsite.verify: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `socialwebsite`.`verify`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
